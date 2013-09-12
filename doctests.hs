70d748e3e0549cd5f6c9a3b155195e5d11dc255f
John Lato <jwlato@tsurucapital.com>
2013-09-11 22:06:00 -0500

    tests: check the two allDirectoryContents variants are equal

--- doctests.hs
+++ doctests.hs
@@ -1,5 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Test.DocTest
+import Control.Applicative
+import System.Posix.Directory.Traversals
 
-main = doctest ["-isrc", "-XOverloadedStrings", "src/System/Posix/FilePath"]
+import Test.DocTest
+import Test.HUnit
+
+main = do
+    doctest
+      [ "-isrc"
+      , "-XOverloadedStrings"
+      , "src/System/Posix/FilePath"
+      ]
+    runTestTT unitTests
+
+unitTests :: Test
+unitTests = test
+    [ TestCase $ do
+        r <- (==) <$> allDirectoryContents "." <*> allDirectoryContents' "."
+        assertBool "allDirectoryContents == allDirectoryContents'" r
+    ]
