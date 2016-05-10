1ce76319fec7561db63124f1e11c093072c3d848
Julian Ospald <hasufell@posteo.de>
2016-05-11 00:27:32 +0200

    Add a getDirectoryContents' version that works on Fd
    This is useful if we don't want to follow symbolic links,
    which 'PosixBS.openDirStream' unfortunately does.
    This can be done by creating a file descriptor via
    our System.Posix.FD.openFd function with 'oNofollow' status
    flag set.
    
    Note that getDirectoryContents' needs special error handling.
    
    We also export fdOpendir.
    
    Theoretically getDirectoryContents could use getDirectoryContents',
    but that might change the way exceptions are thrown and could
    introduce subtle bugs for existing users.
    Instead we refactor the 'loop' function out and make sure it
    gets inlined, so we don't break the benchmarks.

--- src/System/Posix/Directory/Traversals.hs
+++ src/System/Posix/Directory/Traversals.hs
@@ -7,6 +7,7 @@
 module System.Posix.Directory.Traversals (
 
   getDirectoryContents
+, getDirectoryContents'
 
 , allDirectoryContents
 , allDirectoryContents'
@@ -16,6 +17,7 @@ module System.Posix.Directory.Traversals (
 , readDirEnt
 , packDirStream
 , unpackDirStream
+, fdOpendir
 
 , realpath
 ) where
@@ -34,6 +36,7 @@ import System.Posix.Directory.ByteString as PosixBS
 import System.Posix.Files.ByteString
 
 import System.IO.Unsafe
+import System.Posix.IO.ByteString (closeFd)
 import Unsafe.Coerce (unsafeCoerce)
 import Foreign.C.Error
 import Foreign.C.String
@@ -41,6 +44,9 @@ import Foreign.C.Types
 import Foreign.Marshal.Alloc (alloca,allocaBytes)
 import Foreign.Ptr
 import Foreign.Storable
+
+
+
 
 ----------------------------------------------------------
 
@@ -152,8 +158,12 @@ foreign import ccall unsafe "__posixdir_d_type"
 foreign import ccall "realpath"
   c_realpath :: CString -> CString -> IO CString
 
+foreign import ccall unsafe "fdopendir"
+  c_fdopendir :: Posix.Fd -> IO (Ptr ())
+
 ----------------------------------------------------------
 -- less dodgy but still lower-level
+
 
 readDirEnt :: DirStream -> IO (DirType, RawFilePath)
 readDirEnt (unpackDirStream -> dirp) =
@@ -182,6 +192,7 @@ readDirEnt (unpackDirStream -> dirp) =
                     then return (dtUnknown,BS.empty)
                     else throwErrno "readDirEnt"
 
+
 -- |Gets all directory contents (not recursively).
 getDirectoryContents :: RawFilePath -> IO [(DirType, RawFilePath)]
 getDirectoryContents path =
@@ -190,13 +201,39 @@ getDirectoryContents path =
     bracket
       (PosixBS.openDirStream path)
       PosixBS.closeDirStream
-      loop
- where
-  loop dirp = do
-     t@(_typ,e) <- readDirEnt dirp
-     if BS.null e then return [] else do
-       es <- loop dirp
-       return (t:es)
+      _dirloop
+
+
+-- |Binding to @fdopendir(3)@.
+fdOpendir :: Posix.Fd -> IO DirStream
+fdOpendir fd =
+    packDirStream <$> throwErrnoIfNull "fdOpendir" (c_fdopendir fd)
+
+
+-- |Like `getDirectoryContents` except for a file descriptor.
+--
+-- To avoid complicated error checks, the file descriptor is
+-- __always__ closed, even if `fdOpendir` fails. Usually, this
+-- only happens on successful `fdOpendir` and after the directory
+-- stream is closed. Also see the manpage of @fdopendir(3)@ for
+-- more details.
+getDirectoryContents' :: Posix.Fd -> IO [(DirType, RawFilePath)]
+getDirectoryContents' fd = do
+  dirstream <- fdOpendir fd `catchIOError` \e -> do
+    closeFd fd
+    ioError e
+  -- closeDirStream closes the filedescriptor
+  finally (_dirloop dirstream) (PosixBS.closeDirStream dirstream)
+
+
+_dirloop :: DirStream -> IO [(DirType, RawFilePath)]
+{-# INLINE _dirloop #-}
+_dirloop dirp = do
+   t@(_typ,e) <- readDirEnt dirp
+   if BS.null e then return [] else do
+     es <- _dirloop dirp
+     return (t:es)
+
 
 -- | return the canonicalized absolute pathname
 --
